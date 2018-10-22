fd5f504b82955dbd773727f2184debf5fbbded12
shinyay <shinya.com@gmail.com>
2018-10-22 15:36:00 +0900

    Remove obsolete packages imported
    Since Spring depencencies have already deleted

--- src/main/java/org/superbiz/struts/Application.java
+++ src/main/java/org/superbiz/struts/Application.java
@@ -1,16 +1,7 @@
 package org.superbiz.struts;
 
-import com.opensymphony.module.sitemesh.filter.PageFilter;
-import org.apache.struts2.dispatcher.ActionContextCleanUp;
-import org.apache.struts2.dispatcher.FilterDispatcher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-
-import javax.servlet.Filter;
-
-import static java.util.Arrays.asList;import static java.util.Collections.singletonList;
 
 @SpringBootApplication
 public class Application {
